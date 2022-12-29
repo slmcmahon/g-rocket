42a655d3c0d5d4364d185fc06e8e174ece6fdf2c
Stephen McMahon <smcmahon@slb.com>
2022-12-29 09:17:59 -0600

    checkpoint

--- app.py
+++ app.py
@@ -1,76 +1,17 @@
 from __future__ import print_function
-import io
+from grocket import GRocket
 
 import os.path
 import re
-from datetime import datetime
-from collections import namedtuple
 
 from pymongo import MongoClient
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaIoBaseDownload
-
-SCOPES = ['https://www.googleapis.com/auth/drive']
-
-new_files_folder_id = '1f_4r0F3QFDvbdkyHxyPqLt3r--USuCQB'
-processed_files_folder_id = '1ua-Hw-8AVC16TkOyenBIYSaTSKK7nLH1'
+folder_id = '1f_4r0F3QFDvbdkyHxyPqLt3r--USuCQB'
+backup_folder_id = '1ua-Hw-8AVC16TkOyenBIYSaTSKK7nLH1'
 
 dateline_pattern = re.compile("\+.*?(\d{2}-\S{3}-\d{4})\s*[-]?\s*(.*)")
 
-
-def get_files(service):
-    try:
-        query = "trashed = false and parents in '" + new_files_folder_id + "'"
-        #query = ""
-        files = service.files().list(
-            q=query, fields="nextPageToken, files(id, name, mimeType)").execute()
-        return files['files']
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-        return None
-
-
-def get_service():
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    service = build('drive', 'v3', credentials=creds)
-    return service
-
-
-def move_file(service, file_id):
-    file = service.files().get(fileId=file_id, fields='parents').execute()
-    previous_parents = ",".join(file.get('parents'))
-    file = service.files().update(fileId=file_id, addParents=processed_files_folder_id,
-                                  removeParents=previous_parents, fields='id, parents').execute()
-    return file.get('parents')
-
-
-def download_file(service, file_id):
-    request = service.files().export_media(fileId=file_id, mimeType='text/plain')
-    file = io.BytesIO()
-    downloader = MediaIoBaseDownload(file, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        #print(f'Download {int(status.progress() * 100)}.')
-
-    return file.getvalue()
-
+gclient = GRocket(folder_id, backup_folder_id)
 
 def write_to_db(records):
     client = MongoClient(os.getenv('PYMONGO_PERSONAL'))
@@ -79,38 +20,16 @@ def write_to_db(records):
 
 def main():
     try:
-        service = get_service()
-
         records = []
         pdfs = {}
 
-        for f in get_files(service):
+        for f in gclient.get_files():
             if f['mimeType'] == 'application/vnd.google-apps.document':
-                data = download_file(service, f['id'])
-                processed_text = data.decode('utf-8-sig')
-                current = None
-                for l in processed_text.splitlines():
-                    if len(l) == 0:
-                        continue
-
-                    if l.startswith('+'):
-                        dm = dateline_pattern.match(l)
-                        date = dm[1]
-                        header = dm[2].strip()
-
-                        current = {}
-                        current['date'] = date
-                        current['header'] = header
-                        current['content'] = ''
-                        current['pdf'] = f"{f['name'].replace('Transcription ', '')}.pdf"
-                        records.append(current)
-                    else:
-                        if current:
-                            current['content'] += f'{l} '
+                records.append(gclient.parse_text_file(f))
             else:
                 pdfs[f['name']] = f['id']
 
-            move_file(service=service, file_id=f['id'])
+            #gclient.backup_file(file_id=f['id'])
         for r in records:
             r['content'] = r['content'].strip()
             r['pdf_file_id'] = pdfs[r['pdf']]
@@ -119,7 +38,7 @@ def main():
         if len(records) > 0:
             write_to_db(records)
 
-    except HttpError as error:
+    except Exception as error:
         print(f'An error occurred: {error}')
 
 
---
+++ grocket.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+import io
+import os
+import re 
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+
+
+class GRocket():
+
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    DATELINE_PATTERN = re.compile("\+.*?(\d{2}-\S{3}-\d{4})\s*[-]?\s*(.*)")
+
+    def __init__(self, folder_id, backup_folder_id):
+        self.working_folder_id = folder_id
+        self.backup_folder_id = backup_folder_id
+        self._init_drive()
+
+    def _init_drive(self):
+        creds = None
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file(
+                'token.json', self.SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        self.service = build('drive', 'v3', credentials=creds)
+
+    def get_service(self):
+        return self.service
+
+    def get_files(self):
+        try:
+            query = "trashed = false and parents in '" + self.working_folder_id + "'"
+            files = self.service.files().list(
+                q=query, fields="nextPageToken, files(id, name, mimeType)").execute()
+            return files['files']
+        except HttpError as error:
+            print(F'An error occurred: {error}')
+            return None
+
+    def backup_file(self, file_id):
+        file = self.service.files().get(fileId=file_id, fields='parents').execute()
+        previous_parents = ",".join(file.get('parents'))
+        file = self.service.files().update(fileId=file_id, addParents=self.backup_folder_id,
+                                           removeParents=previous_parents, fields='id, parents').execute()
+        return file.get('parents')
+
+    def download_file(self, file_id):
+        request = self.service.files().export_media(
+            fileId=file_id, mimeType='text/plain')
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            #print(f'Download {int(status.progress() * 100)}.')
+
+        return file.getvalue()
+    
+    def parse_text_file(self, file):
+        data = self.download_file(file['id'])
+        processed_text = data.decode('utf-8-sig')
+        entry = {}
+        for l in processed_text.splitlines():
+            if len(l) == 0:
+                continue
+
+            if l.startswith('+'):
+                dm = self.DATELINE_PATTERN.match(l)
+                date = dm[1]
+                header = dm[2].strip()
+                
+                entry['date'] = date
+                entry['header'] = header
+                entry['content'] = ''
+                entry['pdf'] = f"{file['name'].replace('Transcription ', '')}.pdf"
+            else:
+                if entry:
+                    entry['content'] += f'{l} '
+                    
+        return entry
